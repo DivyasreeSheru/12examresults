b8a7ea8ce821b35ba912ad5fb4838d4e8e1e54ea
DivyasreeSheru <166357646+DivyasreeSheru@users.noreply.github.com>
2024-10-18 22:05:37 +0530

    Update apollo1.py

--- apollo1.py
+++ apollo1.py
@@ -80,7 +80,7 @@ def main():
 
     if option == "Home":
         st.header("Welcome to ❤️ Disease Prediction Web ")
-        st.image("https://img.freepik.com/premium-photo/heart-is-chart-with-stethoscope-stethoscope_131346-6087.jpg", caption="Heart Health Matters")
+        st.image("https://cdn.pixabay.com/photo/2023/09/05/08/02/earth-8234588_1280.jpg", caption="Heart Health Matters")
         
         st.write("""
         ### Overview
