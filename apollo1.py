473900ceab7836c57e6d5b115a23dbd3a49e1503
DivyasreeSheru <166357646+DivyasreeSheru@users.noreply.github.com>
2024-10-18 22:07:01 +0530

    Update apollo1.py

--- apollo1.py
+++ apollo1.py
@@ -113,7 +113,7 @@ def main():
 
         """)
         
-        st.image("https://cdn.pixabay.com/photo/2023/09/05/08/02/earth-8234588_1280.jpg", caption="Prevention is Better than Cure")
+        st.image("https://th.bing.com/th/id/OIP.0Rtz6gR6tT84e_p-FyV43AHaEK?rs=1&pid=ImgDetMain", caption="Prevention is Better than Cure")
 
         st.info("Login with **username**: `heartdisease` and **password**: `heart@123` to start using the app.")
 
